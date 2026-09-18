64d94e83a7d36265e521aa4127dd31d478af14d1
graficainmobiliaria-collab <graficainmobiliaria@gmail.com>
2026-09-18 09:47:30 -0300

    Update route_calculator.py

--- route_calculator.py
+++ route_calculator.py
@@ -27,7 +27,8 @@ def calculate_route_distance(addresses):
         time.sleep(1) # Polite delay
         
     coords_str = ';'.join([f'{c[0]},{c[1]}' for c in coords])
-    osrm_url = f'http://router.project-osrm.org/route/v1/driving/{coords_str}?overview=false'
+    # Pedir geometry en formato geojson para poder dibujarla en el mapa
+    osrm_url = f'http://router.project-osrm.org/route/v1/driving/{coords_str}?overview=full&geometries=geojson'
     
     try:
         req = urllib.request.Request(osrm_url, headers={'User-Agent': 'MonopatinApp/1.0'})
@@ -37,10 +38,19 @@ def calculate_route_distance(addresses):
         if data['code'] == 'Ok':
             dist_m = data['routes'][0]['distance']
             dur_s = data['routes'][0]['duration']
+            
+            # Extraer la geometría de la ruta y los waypoints
+            # OSRM devuelve [lon, lat], para Folium necesitamos [lat, lon]
+            geometry_coords = data['routes'][0]['geometry']['coordinates']
+            route_path = [[coord[1], coord[0]] for coord in geometry_coords]
+            waypoints = [[c[1], c[0]] for c in coords]
+            
             return {
                 'success': True, 
                 'distance_km': dist_m / 1000,
-                'duration_min': dur_s / 60
+                'duration_min': dur_s / 60,
+                'route_path': route_path,
+                'waypoints': waypoints
             }
         else:
             return {'success': False, 'error': data.get('message', 'Error en OSRM')}
