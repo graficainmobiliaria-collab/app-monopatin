dfc7593f3f15d85d130db6e3e0c6dde8db0d1989
graficainmobiliaria-collab <graficainmobiliaria@gmail.com>
2026-09-19 18:28:36 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -202,7 +202,8 @@ with tab_planificador:
                             else:
                                 folium.Marker(wp, icon=folium.Icon(color='blue', icon='info-sign'), tooltip=f"Parada {idx}").add_to(m)
                                 
-                        st_folium(m, width=700, height=500)
+                        import streamlit.components.v1 as components
+                        components.html(m._repr_html_(), height=520)
                         
                     else:
                         st.error(f"Error calculando la ruta con el mapa: {res_dist['error']}")
